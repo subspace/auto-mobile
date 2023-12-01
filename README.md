7d199907420660af346a2d533bfa5822e829c56d
abhi3700 <dalvath3700@gmail.com>
2023-12-01 23:59:41 +0530

    Added boilerplate for SDK in TS

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Typescript SDK for Auto
+
+## Build
+
+```sh
+npm run build
+```
---
+++ src/lib/wallet.ts
@@ -0,0 +1,10 @@
+/**
+ * Generate a new wallet with a random seed
+ * returning the SS58 address and the EVM addresses
+ * NOTE: for simplicity, considered only EVM based domains
+ * @returns {SS58Address, [Address]} A new wallet
+ *
+ * @example
+ *
+ */
+export function generateWallet() {}
