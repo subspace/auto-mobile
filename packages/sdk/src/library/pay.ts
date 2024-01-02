4d06031cd3cf93081b00c44ed556bbc38a758d53
abhi3700 <dalvath3700@gmail.com>
2024-01-02 19:20:27 +0530

    pay function added to BE lib for Auto PoC

---
+++ packages/sdk/src/library/pay.ts
@@ -0,0 +1,40 @@
+/**
+ * Payment module for Auto.
+ */
+
+import { ethers, BigNumber } from 'ethers';
+import { NOVA_RPC_URL, SIGNER_PRIVATE_KEY } from './constants';
+
+export async function pay(
+  recipient: string,
+  amount: BigNumber,
+  sender?: ethers.Signer
+): Promise<string> {
+  // provider/client
+  const provider = new ethers.providers.JsonRpcProvider(NOVA_RPC_URL);
+
+  // get the signer if available
+  sender = sender || new ethers.Wallet(`0x${SIGNER_PRIVATE_KEY}`, provider);
+
+  // Get the balance of the signer
+  const balance = await sender.getBalance();
+
+  const gasPrice = await provider.getGasPrice();
+
+  // Check if the signer has enough balance including required gas
+  if (balance.lt(amount.add(gasPrice.mul(21000)))) {
+    throw new Error('Insufficient balance');
+  }
+
+  // Send the transaction
+  const tx = await sender.sendTransaction({
+    to: recipient,
+    value: amount,
+  });
+
+  // Wait for the transaction to be mined
+  await tx.wait();
+
+  // Return the transaction hash
+  return tx.hash;
+}
